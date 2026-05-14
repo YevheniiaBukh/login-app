ad545ba47da7442e86a0b479aec7ef26eb9a8885
Yevheniia <sopli15011970@gmail.com>
2026-05-14 20:04:47 +0200

    login-app

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request, redirect, session
+from flask_bcrypt import Bcrypt
+from models import db, User
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SECRET_KEY'] = 'your-secret-key'
+
+db.init_app(app)
+bcrypt = Bcrypt(app)
+
+@app.route('/')
+def home():
+    if 'user' in session:
+        return render_template('home.html', user=session.get('user'))
+    return redirect('/login')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+
+        hashed_password = bcrypt.generate_password_hash(request.form["password"]).decode('utf-8')
+
+        new_user = User(username=request.form["username"], password=hashed_password)
+        
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect('/login')
+
+    return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = User.query.filter_by(username=request.form["username"]).first()
+
+        if user and bcrypt.check_password_hash(user.password, request.form["password"]):
+            session['user'] = user.username
+            return redirect('/')
+
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect('/login')
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+
